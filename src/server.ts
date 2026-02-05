d72772bb04b0bdc4692776e6253966839203e3e7
setyadigilangp-dev <setyadigilangp239@gmail.com>
2026-02-06 02:57:05 +0700

    final, deploy di email lain untuk be railway

--- src/server.ts
+++ src/server.ts
@@ -7,17 +7,11 @@ dotenv.config();
 const PORT = process.env.PORT || "5001";
 const MONGO_URI = process.env.MONGO_URI || "no-mongo-uri";
 
-if (process.env.VERCEL !== "1") {
-  mongoose
-    .connect(MONGO_URI)
-    .then(() => {
-      console.log("✅ Connected to MongoDB (LOCAL)");
-
-      app.listen(PORT, () => {
-        console.log(`🚀 Server is running locally on port ${PORT}`);
-      });
-    })
-    .catch((error) => {
-      console.error("❌ Error connecting to MongoDB:", error);
+mongoose.connect(MONGO_URI).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
-}
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
